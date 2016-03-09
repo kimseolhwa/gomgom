690dab447dcd87f3abe7ad10b6e98a3a12f247bb
seolhwa <ratfg31@naver.com>
2016-03-09 09:56:45 +0900

    .

--- peekis/src/main/java/bitcamp/java77/service/BoardService.java
+++ peekis/src/main/java/bitcamp/java77/service/BoardService.java
@@ -5,13 +5,8 @@ import java.util.List;
 import bitcamp.java77.domain.Board;
 
 public interface BoardService {
-  List<Board> getBoardList(
-      int pageNo, int pageSize, 
-      String keyword, String align);
   void register(Board board);
-  void remove(int no, String password);
-  void change(Board board);
-  Board retieve(int no);
+
 }
 
 
