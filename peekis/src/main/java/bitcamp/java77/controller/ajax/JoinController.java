7b819a54a294421352b4d09d1887043551237a1d
kimyk0120 <kimyk0120@hotmail.com>
2016-03-15 08:14:46 +0900

    이메일 중복체크 추가

--- peekis/src/main/java/bitcamp/java77/controller/ajax/JoinController.java
+++ peekis/src/main/java/bitcamp/java77/controller/ajax/JoinController.java
@@ -4,9 +4,11 @@ import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -25,18 +27,50 @@ public class JoinController {
   
  
 //  회원가입
-  @RequestMapping(value="join")
-  public AjaxResult join(Join Join) throws Exception {
+  @RequestMapping(value="join", method=RequestMethod.POST)
+  public AjaxResult join(Join Join,HttpSession session) throws Exception {
 	  
 	  System.out.println("컨트롤러 호출" +  Join.getName());
+	  
+	  // db에 등록 
 	  JoinDao.memberJoin(Join);
+	  
+	  // 세션에 등록
+	  session.setAttribute("admin", Join.getEmail());
+	  
 	  
 	return new AjaxResult("success", null);
   }
+
+  
+  @RequestMapping(value="loginCk", method=RequestMethod.POST)
+  public AjaxResult join(Join Join) throws Exception {
+	  
+	  System.out.println("로그인 체크 컨트롤러 호출, 이메일값 : " +  Join.getEmail());
+	  
+	  // db에서 조회
+	    
+	  int checkedEmailCnt  =  JoinDao.loginCheck(Join);
+	  System.out.println(" 로긴체크하면서 다오에서 넘어온 값 : " +  checkedEmailCnt);
+
+	
+	  if(checkedEmailCnt>0){
+		  // 이메일 중복될 때 
+		  return new AjaxResult("중복이메일있음", checkedEmailCnt);
+	  }else{
+		  // 이메징 중복 없을 때 
+		  return new AjaxResult("ok", checkedEmailCnt);
+	  }
+	  
+  }
+  
   
   
   @RequestMapping(value="add", method=RequestMethod.POST)
-  public AjaxResult add(Join Join/*, MultipartFile file*/) throws Exception {
+  public AjaxResult add(Join Join) throws Exception {
+	  
+	  
+	  
     /*
     if (file.getSize() > 0) {
       String newFileName = MultipartHelper.generateFilename(file.getOriginalFilename());  
