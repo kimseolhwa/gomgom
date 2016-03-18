330f9f77d907ac2002841452f032a69b1194975f
cheonok <cheon_ok@naver.com>
2016-03-18 09:10:31 +0900

    인덱스

---
+++ peekis/src/main/java/bitcamp/java77/domain/IndexStatus.java
@@ -0,0 +1,39 @@
+package bitcamp.java77.domain;
+
+public class IndexStatus
+{
+	private int totalCnt;
+	private int todayCnt;
+	private int goalCnt;
+
+	public int getTotalCnt()
+	{
+		return totalCnt;
+	}
+
+	public void setTotalCnt(int totalCnt)
+	{
+		this.totalCnt = totalCnt;
+	}
+
+	public int getTodayCnt()
+	{
+		return todayCnt;
+	}
+
+	public void setTodayCnt(int todayCnt)
+	{
+		this.todayCnt = todayCnt;
+	}
+
+	public int getGoalCnt()
+	{
+		return goalCnt;
+	}
+
+	public void setGoalCnt(int goalCnt)
+	{
+		this.goalCnt = goalCnt;
+	}
+
+}
