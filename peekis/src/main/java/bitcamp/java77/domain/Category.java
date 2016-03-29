beb359b48ddae21c0386be2d94fe887ce916f3a8
cheonok <cheon_ok@naver.com>
2016-03-29 09:25:58 +0900

    카테고리 리스트 표시 사진표시 야매

--- peekis/src/main/java/bitcamp/java77/domain/Category.java
+++ peekis/src/main/java/bitcamp/java77/domain/Category.java
@@ -8,7 +8,10 @@ public class Category implements Serializable {
 	protected int cNo; // 카테고리 번호 
 	protected String name;  // 카테고리 명 
 	protected int uNo; // 유저 번호 
-	protected String path; // 카테고리에 나올 사진
+	protected String path1; // 카테고리에 나올 사진
+	protected String path2; // 카테고리에 나올 사진
+	protected String path3; // 카테고리에 나올 사진
+	protected String path4; // 카테고리에 나올 사진
 	
 
 	//getter,setter
@@ -30,13 +33,38 @@ public class Category implements Serializable {
 	public void setuNo(int uNo) {
 		this.uNo = uNo;
 	}
-	public String getFpath()
+	public String getPath1()
 	{
-		return path;
+		return path1;
 	}
-	public void setFpath(String fpath)
+	public void setPath1(String path1)
 	{
-		this.path = path;
+		this.path1 = path1;
 	}
+	public String getPath2()
+	{
+		return path2;
+	}
+	public void setPath2(String path2)
+	{
+		this.path2 = path2;
+	}
+	public String getPath3()
+	{
+		return path3;
+	}
+	public void setPath3(String path3)
+	{
+		this.path3 = path3;
+	}
+	public String getPath4()
+	{
+		return path4;
+	}
+	public void setPath4(String path4)
+	{
+		this.path4 = path4;
+	}
+	
 
 }
